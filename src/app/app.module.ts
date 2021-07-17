dcd8aaf519697644238409e5e62ec8467a35d9f3
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-17 23:10:44 +0700

    app bars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// Routing
 import { AppRoutingModule } from './app-routing.module';
-
+// Material section
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -15,11 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    //routing
     AppRoutingModule,
     BrowserAnimationsModule,
+    //material
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
 
   ],
   providers: [],
