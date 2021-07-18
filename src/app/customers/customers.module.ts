ed4458148a4eea606396cb030715974cca6cf1e7
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-18 14:39:47 +0700

    cards material

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
-
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    CustomerListComponent
+    CustomerListComponent,
+    CustomerCardComponent
   ],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class CustomersModule { }
