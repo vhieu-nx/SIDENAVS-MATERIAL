6841a9f634c0896b518bda81dd4bee7b379df4e5
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-18 15:11:46 +0700

    steppers material

--- src/app/messages/messages-routing.module.ts
+++ src/app/messages/messages-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomerListComponent} from "../customers/customer-list/customer-list.component";
+import {MessageNewComponent} from "./message-new/message-new.component";
 
 const routes: Routes = [
   {
     path: '',
     component: CustomerListComponent
+  },
+  {
+    path: 'new',
+    component: MessageNewComponent
   }
 ];
 
--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {FormsModule,ReactiveFormsModule} from "@angular/forms";
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
+import { MessageNewComponent } from './message-new/message-new.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
   declarations: [
-    MessageListComponent
+    MessageListComponent,
+    MessageNewComponent
   ],
   imports: [
     CommonModule,
-    MessagesRoutingModule
+    MessagesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+    , MatStepperModule, MatIconModule
   ]
 })
 export class MessagesModule { }
